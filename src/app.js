093e93648b9fa139a97806354dd7964fa0b0f194
SrCinque <filipe.cinque@gmail.com>
2024-09-13 10:48:38 -0300

    Configurações de banco de dados

--- src/app.js
+++ src/app.js
@@ -1,6 +1,24 @@
-const express = require('express')
+import express from 'express';
+import { db } from './configDB.js'
 const app = express();
 
+
+
+
+app.use(express.json());
+db();
+
+
+app.post('/list', (req,res)=>{
+    console.log(req.body)
+    res.status(200).json({
+        "statusCode": "200"
+    })
+    /*res.json({
+        "statusCode": "404"
+    })*/
+
+})
 
 app.get('/', function(req,res){
     res.send('Teste se server')
